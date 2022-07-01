25050eacca002cdd874ab4276adbab474e6f23c9
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-07-01 13:34:28 -0500

    just started

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// from open weather api 
+
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} 
+// example 
+// https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key} lat is latitude lon is logitude
