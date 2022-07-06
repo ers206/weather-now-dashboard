28064cd4a4ab07e281d147b1e4fcfabb9a745963
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-07-06 11:21:29 -0500

    need button to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,20 @@
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} 
 // example 
 // https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key} 
-var city = "portland"
+var city = "Phoenix"
 var weatherApiCity = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid=bb9820e3d7aab8059f144ee6dd8eca00"
 var citiesSearched = document.querySelector("#selected-cities")
-// var dailyEl = document.querySelector("#5day")
+var daily1 = document.querySelector("#day1") 
+var daily2 = document.querySelector("#day2") 
+var daily3 = document.querySelector("#day3") 
+var daily4 = document.querySelector("#day4") 
+var daily5 = document.querySelector("#day5") 
 var todayTemp = document.querySelector("#tempContainer")
 var todayWind = document.querySelector("#windContainer") 
 var todayHumidity = document.querySelector("#humidityContainer")
 var todayFeelsLike = document.querySelector("#feelsLikeContainer")
 var currentCity = document.querySelector("#current-city")
-
+var cityName1 =  document.querySelector("#cityName")
 fetch(weatherApiCity)
     .then (function(response) {
         if(response.ok) {
@@ -43,10 +47,22 @@ function handleFrontend(city, dataObj) {
     todayHumidity.textContent = "humidity: " + dataObj.current.humidity + "%"
     todayFeelsLike.textContent = "Feels like: " + dataObj.current.feels_like + "°F"
     currentCity.textContent = "Weather for: " + city 
+    cityName1.textContent =  "5 day forecast for: " + city
     console.log(dataObj.daily[3].temp.day)
+    // all smooshed together on page 
+    daily1.textContent = "High Temp: " + dataObj.daily[1].temp.max + "°F" + "Low Temp: " + dataObj.daily[1].temp.min + "°F" + "wind speed: "+ dataObj.daily[1].wind_speed + "mph" + "humidity: " + dataObj.daily[1].humidity + "%"
+    daily2.textContent = "High Temp: " + dataObj.daily[2].temp.max + "°F" + "Low Temp: " + dataObj.daily[2].temp.min + "°F" + "wind speed: "+ dataObj.daily[2].wind_speed + "mph" + "humidity: " + dataObj.daily[2].humidity + "%"
+    daily3.textContent = "High Temp: " + dataObj.daily[3].temp.max + "°F" + "Low Temp: " + dataObj.daily[3].temp.min + "°F" + "wind speed: "+ dataObj.daily[3].wind_speed + "mph" + "humidity: " + dataObj.daily[3].humidity + "%"
+    daily4.textContent = "High Temp: " + dataObj.daily[4].temp.max + "°F" + "Low Temp: " + dataObj.daily[4].temp.min + "°F" + "wind speed: "+ dataObj.daily[4].wind_speed + "mph" + "humidity: " + dataObj.daily[4].humidity + "%"
+    daily5.textContent = "High Temp: " + dataObj.daily[5].temp.max + "°F" + "Low Temp: " + dataObj.daily[5].temp.min + "°F" + "wind speed: "+ dataObj.daily[5].wind_speed + "mph" + "humidity: " + dataObj.daily[5].humidity + "%"
 }
+    
 // weatherClick()
 // var weatherApiCity = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid=bb9820e3d7aab8059f144ee6dd8eca00"
+
+
+// from.submit() 
+
 
 // var button = select("#submit-btn"); 
 // button.mousePressed(weatherClick); 
